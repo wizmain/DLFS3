991dd9e45af283e0a4c10b2a4572371ffc859760
wizmain <wizmain@gmail.com>
2022-02-07 15:02:21 +0900

    a

--- steps/step01.py
+++ steps/step01.py
@@ -1,20 +1,38 @@
 import sys
 import numpy as np
+import unittest
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+
         self.data = data
         self.grad = None
         self.creator = None
 
     def set_creator(self, func):
         self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop() # 함수를 가져온다.
+            x, y = f.input, f.output # 함수의 입력과 출력을 가져온다.
+            x.grad = f.backward(y.grad) # backward 메서드를 호출한다.
+
+            if x.creator is not None:
+                funcs.append(x.creator) #하나 앞의 함수를 리스트에 추가한다.
        
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self) # 출력 변수에 창조자를 설정한다.
         self.input = input # 입력 변수를 기억(보관)한다.
         self.output = output # 출력도 저장한다.
@@ -45,18 +63,44 @@ class Square(Function):
         gx = 2 * x * gy
         return gx
 
+def square(x):
+    return Square()(x)
 
-A = Square()
-B = Exp()
-C = Square()
+def exp(x):
+    return Exp()(x)
 
-x = Variable(np.array(0.5))
-a = A(x)
-b = B(a)
-y = C(b)
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
-y.grad = np.array(1.0)
-b.grad = C.backward(y.grad)
-a.grad = B.backward(b.grad)
-x.grad = A.backward(a.grad)
-print(x.grad)
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1)) # 무작위 입력값 생성
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+unittest.main()

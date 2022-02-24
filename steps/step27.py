4891cd20abc953171178ebfc0079f2ae08e4de83
wizmain <wizmain@gmail.com>
2022-02-24 09:42:46 +0900

    test

--- steps/step27.py
+++ steps/step27.py
@@ -4,6 +4,7 @@ if '__file__' in globals():
 import numpy as np
 from dezero import Variable
 from dezero import Function
+import math
 
 class Sin(Function):
     def forward(self, x):
@@ -18,8 +19,18 @@ class Sin(Function):
 def sin(x):
     return Sin()(x)
 
+def my_sin(x, threshold=0.0001):
+    y = 0
+    for i in range(100000):
+        c = (-1)**i/math.factorial(2*i+1)
+        t = c * x ** (2 * i + 1)
+        y = y + t
+        if abs(t.data) < threshold:
+            break
+    return y
+
 x = Variable(np.array(np.pi/4))
-y = sin(x)
+y = my_sin(x)
 y.backward()
 
 print(y.data)
